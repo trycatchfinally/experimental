eb5a2cea50aaeb7461d55fb3f3d92c9a1cbc955d
John Lambert <jlamb.personal@gmail.com>
2025-08-09 16:52:07 -0700

    ch 5 integration test

---
+++ rpov/tests/ch5-spheres.rs
@@ -0,0 +1,56 @@
+mod test {
+    use std::{f64::consts::PI, string::String};
+
+    use num_traits::ToPrimitive;
+    use rpov::{
+        Tuple4,
+        canvas::Canvas,
+        colors::COLOR_RED,
+        matrices::{Matrix, Matrix4},
+        point,
+        spheres::Sphere,
+        transformations::{rotation_z, scaling, shearing},
+    };
+
+    fn run_example(name: &str, transform: Matrix4<f64>) {
+        let ray_origin = point(0.0, 0.0, -5.0);
+        let wall_z = -10.0;
+        let wall_size = 7.0;
+        let canvas_pixels = 100;
+        let pixel_size = wall_size / (canvas_pixels.to_f32().unwrap());
+        let half = wall_size / 2.0;
+        let mut c = Canvas::new(canvas_pixels, canvas_pixels);
+        let color = COLOR_RED;
+        let shape = Sphere::with_transform(transform);
+
+        for y in 0..canvas_pixels {
+            let world_y = half - pixel_size * y.to_f32().unwrap();
+            for x in 0..canvas_pixels {
+                let world_x = -half + pixel_size * x.to_f32().unwrap();
+                let position: Tuple4<f64> = point(world_x.into(), world_y.into(), wall_z.into());
+                let r = rpov::rays::ray(ray_origin, (position - ray_origin).normalize());
+                let xs = shape.intersect(r);
+
+                if xs.len() > 0 {
+                    c.write_pixel(x, y, color);
+                }
+            }
+        }
+        let path = format!("tests/out-ch5-{name}.ppm");
+        std::fs::write(&path, c.to_ppm()).expect("Unable to write file");
+    }
+
+    #[test]
+    fn ch5_putting_it_together() {
+        let sx = scaling(0.5, 1.into(), 1.into());
+        run_example("identity", Matrix4::identity());
+        run_example("scaling-y", scaling(1.into(), 0.5, 1.into()));
+        run_example("scaling-x", sx);
+
+        let shr = rotation_z(PI / 4.0) * scaling(0.5, 1.into(), 1.into());
+        run_example("shrink-rotation", shr);
+
+        let skr = shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0) * sx;
+        run_example("shearing", skr);
+    }
+}
