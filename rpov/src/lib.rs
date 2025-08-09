5300b03b3b29111d0f617d81427d211cefd9792a
John Lambert <jlamb.personal@gmail.com>
2025-08-09 15:35:58 -0700

    add sphere

--- rpov/src/lib.rs
+++ rpov/src/lib.rs
@@ -2,6 +2,7 @@ pub mod canvas;
 pub mod colors;
 pub mod matrices;
 pub mod rays;
+pub mod spheres;
 pub mod transformations;
 pub mod tuples;
 
---
+++ rpov/src/spheres.rs
@@ -0,0 +1,155 @@
+use crate::rays::Ray;
+use crate::{TupleElement, point};
+use std::sync::atomic::{AtomicU64, Ordering};
+
+static SPHERE_ID_COUNTER: AtomicU64 = AtomicU64::new(0);
+
+#[derive(Debug)]
+pub struct Sphere {
+    pub id: u64,
+}
+
+impl PartialEq for Sphere {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Sphere {
+    pub fn new() -> Self {
+        Self {
+            id: SPHERE_ID_COUNTER.fetch_add(1, Ordering::SeqCst),
+        }
+    }
+
+    pub fn intersect<'a>(&'a self, r: Ray<f64>) -> Vec<Intersection<'a, f64>> {
+        let sphere_to_ray = r.origin - point(0.0, 0.0, 0.0);
+
+        let a = r.direction.dot(r.direction);
+        let b = 2.0 * r.direction.dot(sphere_to_ray);
+        let c = sphere_to_ray.dot(sphere_to_ray) - 1.0;
+
+        let discriminant = b.powi(2) - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            return vec![];
+        }
+
+        let t1 = (-b - discriminant.sqrt()) / (2.0 * a);
+        let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
+
+        if t1 > t2 {
+            return vec![Intersection::new(t2, self), Intersection::new(t1, self)];
+        }
+
+        vec![Intersection::new(t1, self), Intersection::new(t2, self)]
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Intersection<'a, T: TupleElement> {
+    pub t: T,
+    pub object: &'a Sphere,
+}
+
+impl<'a, T: TupleElement> Intersection<'a, T> {
+    pub fn new(t: T, object: &'a Sphere) -> Self {
+        Self { t, object }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::rays::ray;
+    use crate::vector;
+
+    // Scenario: A ray intersects a sphere at two points
+    //   Given r ← ray(point(0, 0, -5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = 4.0
+    //     And xs[1] = 6.0
+    #[test]
+    fn test_ray_intersects_sphere_at_two_points() {
+        let r = ray(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+    }
+
+    // Scenario: A ray intersects a sphere at a tangent
+    //   Given r ← ray(point(0, 1, -5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = 5.0
+    //     And xs[1] = 5.0
+    #[test]
+    fn test_ray_intersects_sphere_at_a_tangent() {
+        let r = ray(point(0.0, 1.0, -5.0), vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 5.0);
+        assert_eq!(xs[1].t, 5.0);
+    }
+
+    // Scenario: A ray misses a sphere
+    //   Given r ← ray(point(0, 2, -5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 0
+    #[test]
+    fn test_ray_misses_a_sphere() {
+        let r = ray(point(0.0, 2.0, -5.0), vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+        assert_eq!(xs.len(), 0);
+    }
+
+    // Scenario: A ray originates inside a sphere
+    //   Given r ← ray(point(0, 0, 0), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = -1.0
+    //     And xs[1] = 1.0
+    #[test]
+    fn test_ray_originates_inside_a_sphere() {
+        let r = ray(point(0.0, 0.0, 0.0), vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -1.0);
+        assert_eq!(xs[1].t, 1.0);
+    }
+
+    // Scenario: A sphere is behind a ray
+    //   Given r ← ray(point(0, 0, 5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = -6.0
+    //     And xs[1] = -4.0
+    #[test]
+    fn test_sphere_is_behind_a_ray() {
+        let r = ray(point(0.0, 0.0, 5.0), vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -6.0);
+        assert_eq!(xs[1].t, -4.0);
+    }
+
+    #[test]
+    fn two_spheres_have_different_ids() {
+        let s1 = Sphere::new();
+        let s2 = Sphere::new();
+        assert_ne!(s1.id, s2.id);
+    }
+}
